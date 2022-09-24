7a413dbcd59d8e52c56567276ebecbfc26fc170b
KIWI790 <85770245+KIWI790@users.noreply.github.com>
2022-09-25 12:06:49 +1300

    DEMO

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,37 +7,37 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "  pelican";
+const description = "A collection of 3D digital art, made on blender. Travel around New Zealand up high.";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 2500,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "New Zealand" },
+      { name: "Feet" },
+      { name: "Body" },
+      { name: "Head" },
+      { name: "Addon" },
+      
     ],
   },
 ];
 
-const shuffleLayerConfigurations = true;
+const shuffleLayerConfigurations = false;
 
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 1080,
+  height: 1080,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://twitter.com/BerrymanBrendon", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
